3f2422257d02d96a256057600608498cb9876edb
Thijs Krooswijk <t.krooswijk@gmail.com>
2022-01-24 23:23:55 -0800

    chore: add classname styles

--- src/components/topic.tsx
+++ src/components/topic.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'gatsby'
 import { ContentfulAction } from './models/contentful-action'
 import { ContentfulAsset } from './models/contentful-asset'
 import { ContentfulTopic } from './models/contentful-topic'
@@ -20,9 +21,11 @@ export const Topic = ({ model, options }: TopicProps) => {
   } = model
 
   return (
-    <>
+    <div className="text-center p-5">
       {options.icon && icon && <div>{icon}</div>}
-      {options.heading && heading && <h2>{heading}</h2>}
+      {options.heading && heading && (
+        <h2 className="text-2xl font-bold">{heading}</h2>
+      )}
       {options.abstract && abstract && <p>{abstract}</p>}
       {options.media &&
         mediaCollection &&
@@ -44,12 +47,12 @@ export const Topic = ({ model, options }: TopicProps) => {
             } = action
 
             return (
-              <a key={id + index} href={slug}>
+              <Link key={id + index} to={slug} className='w-full m-3 flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10'>
                 {action.heading}
-              </a>
+              </Link>
             )
           }
         )}
-    </>
+    </div>
   )
 }
