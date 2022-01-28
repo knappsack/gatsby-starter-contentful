806ae23cbfba0a44340a70ecd5976f7ed9820968
Thijs Krooswijk <t.krooswijk@gmail.com>
2022-01-27 17:15:18 -0800

    chore: rename type get-types-of

--- src/lib/get-types-of.ts
+++ src/lib/get-types-of.ts
@@ -1,11 +1,11 @@
 type ReactJSXIntrinsicElements = JSX.IntrinsicElements
-type JSXIntrinsicElements = keyof ReactJSXIntrinsicElements
+export type JSXIntrinsicElements = keyof ReactJSXIntrinsicElements
 
 /**
  * Get any IntrinsicElements Prop Types
  * @example type Component = GetTypesOf['div']
  */
-export type GetPropTypesOf = {
+export type GetTypesOf = {
   [K in JSXIntrinsicElements]: ReactJSXIntrinsicElements[K] & {
     /**
      * Specify that a standard HTML element should behave like a defined custom built-in element
