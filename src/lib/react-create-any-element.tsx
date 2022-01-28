ecc613def236fa69f0643ddd634bb1559bc4a8d8
Thijs Krooswijk <t.krooswijk@gmail.com>
2022-01-27 17:18:26 -0800

    chore: create components with AnyElement

---
+++ src/lib/react-create-any-element.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+type AnyElementProps = {
+  is: keyof JSX.IntrinsicElements
+  className?: string
+}
+
+export const AnyElement: React.FC<AnyElementProps> = ({ children, ...props }) => {
+  const type = props.is || 'div'
+  const attributes = Object.assign({}, props)
+  delete attributes.is
+  /** 
+   * Emotion CSS-in-JS helper
+   * ------------------------
+   * Copy className to new element. This allows for creating 
+   * components that inherit styling from the base component.
+   */
+  attributes.className = props.className
+  return React.createElement(type, attributes, children)
+}
