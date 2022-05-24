7acd245dd521076d7adc19682a2f4f8e9d28266f
Thijs Krooswijk <t.krooswijk@gmail.com>
2022-05-24 16:26:59 -0700

    chore: create variant Contracts

---
+++ src/styles/contracts.ts
@@ -0,0 +1,23 @@
+export const topicVariantContract = {
+  block: {},
+  card: {},
+  divider: {},
+  featured: {},
+  gallery: {},
+  headline: {},
+  hero: {},
+  image: {},
+  quote: {},
+  showcase: {},
+  video: {},
+}
+
+export const textVariantContract = {
+  text: {},
+}
+
+export const navigationVariantContract = {
+  header: {},
+  footer: {},
+  sitemap: {},
+}
