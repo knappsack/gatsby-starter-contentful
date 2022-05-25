e33f8b8d8c69ef5bccaa734d278ab881458dd9ae
Thijs Krooswijk <t.krooswijk@gmail.com>
2022-05-25 16:32:10 -0700

    chore: add second breakpoint

--- src/styles/media-query.ts
+++ src/styles/media-query.ts
@@ -1,6 +1,6 @@
 import facepaint from "facepaint"
 
-const breakpoints = [768]
+const breakpoints = [734, 1068]
 
 export const mediaQuery = facepaint(
   breakpoints.map(breakPoint => `@media (min-width: ${breakPoint}px)`)
