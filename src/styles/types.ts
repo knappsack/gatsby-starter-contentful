078c95c75b49102571d0feae3441da2ed9a9a15a
Thijs Krooswijk <t.krooswijk@gmail.com>
2022-05-25 16:31:51 -0700

    chore: create Options types

--- src/styles/types.ts
+++ src/styles/types.ts
@@ -1,3 +1,5 @@
 import { CSSObject } from "@emotion/react"
 
 export type Variants<Variant extends string> = Record<Variant, CSSObject>
+
+export type Options<Option extends string> = Partial<Record<Option, boolean>>
