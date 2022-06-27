b40a46120064a26825f7289a62020567fa467afb
Thijs Krooswijk <t.krooswijk@gmail.com>
2022-06-27 12:24:41 -0700

    chore: implement Emotion Cache

--- src/components/layout/root-element.tsx
+++ src/components/layout/root-element.tsx
@@ -1,14 +1,22 @@
 import * as React from "react"
 
+import { CacheProvider } from '@emotion/react'
+import createCache from '@emotion/cache'
 import { AppProvider } from "../../lib/app-provider"
 import { RefreshData } from "../../lib/refresh-data"
+
+const emotionCache = createCache({
+  key: 'knappsack',
+})
 
 type RootElementProps = { children: React.ReactNode }
 
 const RootElement = ({ children }: RootElementProps) => (
   <AppProvider value={undefined}>
-    {children}
-    <RefreshData />
+    <CacheProvider value={emotionCache}>
+      {children}
+      <RefreshData />
+    </CacheProvider>
   </AppProvider>
 )
 
