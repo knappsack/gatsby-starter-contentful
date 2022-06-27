1143fa198d0707b8116ce5bd4b22e75be272737d
Thijs Krooswijk <t.krooswijk@gmail.com>
2022-06-27 12:25:16 -0700

    chore: improve Create Any Element

--- src/lib/create-any-element.tsx
+++ src/lib/create-any-element.tsx
@@ -9,13 +9,6 @@ export const Any = (props: AnyProps) => {
   const type = props.is || "div"
   const attributes = Object.assign({}, props)
   delete attributes.is
-  /**
-   * Emotion CSS-in-JS helper
-   * ------------------------
-   * Copying the className to create element
-   * inherit styles from the base component.
-   */
-  attributes.className = props.className
   return React.createElement(type, attributes, props.children)
 }
 
@@ -24,7 +17,6 @@ export const AnyForwardRef = React.forwardRef(
     const type = props.is || "div"
     const attributes = Object.assign({}, props)
     delete attributes.is
-    attributes.className = props.className
     return React.createElement(type, { ...attributes, ref }, props.children)
   }
 )
